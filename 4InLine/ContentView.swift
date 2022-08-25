5fc525c4307572dd2f36d4b046040f93a25493ab
emmanuel Z <emmanuel1997@gmailcom>
2022-08-24 20:53:43 -0500

    fist commit

---
+++ 4InLine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  4InLine
+//
+//  Created by Emmanuel Zambrano Quitian on 8/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 4InLine/_InLineApp.swift
@@ -0,0 +1,17 @@
+//
+//  _InLineApp.swift
+//  4InLine
+//
+//  Created by Emmanuel Zambrano Quitian on 8/24/22.
+//
+
+import SwiftUI
+
+@main
+struct _InLineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
